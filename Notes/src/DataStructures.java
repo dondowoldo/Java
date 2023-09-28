fa9fde37a7e4a852da83032a32a1d5d0f6f00cfa
Daniel Valasek <daniel.valasek@yahoo.com>
2023-09-28 19:15:12 +0200

    uprava indentace

--- Notes/src/DataStructures.java
+++ Notes/src/DataStructures.java
@@ -27,9 +27,9 @@ public class DataStructures {
         //////////////////////////////// 2D ARRAYS ////////////////////////////////
 
         int[][] matrix = new int[2][3];                     //          [0,0][0,1][0,2]
-        //          [1,0][1,1][1,2]
+                                                            //          [1,0][1,1][1,2]
 
-        int[][] matrixFilled = new int[][]{                //          [0,0][0,1][0,2]
+        int[][] matrixFilled = new int[][]{                   //          [0,0][0,1][0,2]
                 {1, 2, 3},                                    //          [1,0][1,1][1,2]
                 {4, 5, 6},                                    //          [2,0][2,1][2,2]
                 {7, 8, 9}                                     //
@@ -37,7 +37,7 @@ public class DataStructures {
 
         int[][] matrixLoop = new int[3][4];                             // Příklad jak naplnit 2D array hodnotami z
         int[] values = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};      // klasického arraye pomocí proměnné valueIndex, kterou
-        int valueIndex = 0;                                         // posouváme o 1 kdykoli přidáme hodnotu do 2D arraye.
+        int valueIndex = 0;                                             // posouváme o 1 kdykoli přidáme hodnotu do 2D arraye.
 
         for (int i = 0; i < matrixLoop.length; i++) {                   //      2D array pak bude vypadat takto:
             for (int j = 0; j < matrixLoop[i].length; j++) {            //      [1][2][3][4]
@@ -73,9 +73,9 @@ public class DataStructures {
         }
 
         int d = listHodnot2.get(2);                                 // Vytáhne prvek z listu na indexu "2" a uloží do proměnné "d"
-        listHodnot2.remove(2);                                      // Odstraní prvek z listu na indexu "2"
+        listHodnot2.remove(2);                                // Odstraní prvek z listu na indexu "2"
         listHodnot2.add(5);                                         // Přidá "5" na konec listu
-        listHodnot2.add(1, 5);                                      // Přídá "5" na index 1 v listu a posune zbytek
+        listHodnot2.add(1, 5);                        // Přídá "5" na index 1 v listu a posune zbytek
         listHodnot2.addAll(listHodnot);                             // Přidá do listu všechny prvky z jiné kolekce
         listHodnot2.size();                                         // Vrátí počet prvků v listu (ekvivalent .length u arrayů)
         listHodnot2.isEmpty();                                      // Vrátí boolean true, pokud je list prázdný. Jinak false.
@@ -84,12 +84,12 @@ public class DataStructures {
         //listHodnot2.clear();                                      // Kompletně vymaže všechny prvky z listu
 
         List<Integer> subListCAREFUL = listHodnot2.subList(0, 5);   // Vrátí pohled na sublist z původního listu, mezi indexy
-        // spodní index - inclusive , vrchní index - exclusive !!
-        // POZOR !! Nevytváří nový list. Pokud subList změníme, změny se
-        // projeví i v originále. Pokud chceme list nový, musíme použít "new"
+                                                                    // spodní index - inclusive , vrchní index - exclusive !!
+                                                                    // POZOR !! Nevytváří nový list. Pokud subList změníme, změny se
+                                                                    // projeví i v originále. Pokud chceme list nový, musíme použít "new"
 
         List<Integer> subListNEW = new ArrayList<>(listHodnot2.subList(0, 5)); // Příklad vytvoření nového sublistu, který můžeme měnit
-        // bez následků v originále.
+                                                                                // bez následků v originále.
 
         listHodnot2.indexOf(8);                                     // Vrátí index, na kterém se nachází daný prvek (-1 pokud tam není)
         listHodnot2.lastIndexOf(10);                                // Vrátí index posledního výskytu daného prvku (-1 pokud tam není)
@@ -108,12 +108,14 @@ public class DataStructures {
         nakup.put("maslo", 3);                                 // Přidá do mapy další pár (k+v)
         nakup.size();                                          // Vrací počet párů v mapě
         nakup.get("vejce");                                    // Vrátí hodnotu záznamu s klíčem "vejce"
-        nakup.remove("chleba");                                // Vymaže celý záznam s klíčem "chleba"
+        nakup.remove("chleba");                           // Vymaže celý záznam s klíčem "chleba"
+
 
         nakup.replace("mliko", 4);                             // Přepíše hodnutu záznamu s klíčem "mliko" na novou
-        nakup.replace("mliko", 4, 1);                          // Vrací boolean. Přepíše hodnotu záznamu s klíčem "mliko" na novou, ale pouze
-        // v případě, že se původní hodnota shoduje. Jinak záznam nezmění,
-        // a metoda vrátí boolean false.
+
+        nakup.replace("mliko", 4, 1);     // Vrací boolean. Přepíše hodnotu záznamu s klíčem "mliko" na novou, ale pouze
+                                                                // v případě, že se původní hodnota shoduje. Jinak záznam nezmění,
+                                                                // a metoda vrátí boolean false.
 
         nakup.containsKey("rohlik");                            // Vrací boolean true, pokud Mapa obsahuje záznam s klíčem "rohlik". Jinak false
         nakup.values();                                         // Vrací kolekci všech hodnot [12, 1, 3] v Mapě. Ale nevíme, k čemu patří
@@ -133,8 +135,8 @@ public class DataStructures {
 
         for (Map.Entry zaznam : nakup.entrySet()) {
             if (zaznam.getValue().equals(3)) {
-                System.out.println(zaznam);                 // vytiskne celý záznam
-                System.out.println(zaznam.getKey());        // vytiskne klíč
+                System.out.println(zaznam);                                                  // vytiskne celý záznam
+                System.out.println(zaznam.getKey());                                         // vytiskne klíč
                 docasnaMapa.put(zaznam.getKey().toString(), (Integer) zaznam.getValue());    // přidá záznam do jiné mapy
             }
         }
@@ -152,6 +154,21 @@ public class DataStructures {
                 "Honza", honzaNakup,
                 "Jana", janaNakup
         ));
+
+                // ( Alternativní způsob přidání pokud samotné nákupy nemáme ve vlastní mapě : )
+         /*
+                                     Map<String, Map<String, Integer>> nakupProVicLidi = new HashMap<>(Map.of(
+                                             "Honza", Map.of(
+                                                     "mliko", 4,
+                                                     "chleba", 1
+                                                    ),
+                                             "Jana", Map.of(
+                                                     "mliko", 2,
+                                                     "vejce", 12
+                                                    )
+                                             ));
+          */
+
 
         int pocetMlika = nakupProVicLidi.get("Honza").get("mliko");             // Vrací počet mlíka pro Honzu a ukládá do proměnné
         System.out.println(pocetMlika);
@@ -185,10 +202,11 @@ public class DataStructures {
         String denTestu = "";
         for (int i = 0; i < znamkyZMatematiky.get("Honza").size(); i++) {       // Iterujeme skrz všechny Honzovi známky
             if (znamkyZMatematiky.get("Honza").get(i).equals(4)) {              // a hledáme prvek v listu který se rovná 4
-                denTestu = dnyVTydnu.get(i);                                // Když najdeme index dne, získáme Stringovou hodnotu
-            }                                                               // (z pomocné mapy výše), odpovídající indexu a následně
-        }                                                                   // uložíme do proměnné denTestu
+                denTestu = dnyVTydnu.get(i);                                    // Když najdeme index dne, získáme Stringovou hodnotu
+            }                                                                   // (z pomocné mapy výše), odpovídající indexu a následně
+        }                                                                       // uložíme do proměnné denTestu
 
-        System.out.println(denTestu);                                       // Tiskneme den kdy Honza dostal z testu za 4.
+        System.out.println(denTestu);                                           // Tiskneme den kdy Honza dostal z testu za 4.
+
     }
 }
