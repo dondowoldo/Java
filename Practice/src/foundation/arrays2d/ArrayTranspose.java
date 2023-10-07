6466461b5ebf16bb4b2ed2ea7048d06b49a5b2c2
Daniel Valasek <daniel.valasek@yahoo.com>
2023-10-07 10:42:30 +0200

    ArrayTranspose + test

---
+++ Practice/src/foundation/arrays2d/ArrayTranspose.java
@@ -0,0 +1,20 @@
+package foundation.arrays2d;
+
+public class ArrayTranspose {
+    //Write a method called transpose(int[][] matrix) which takes a 2D integer array as input and returns its transpose.
+    // The transpose of a matrix is a new matrix in which the rows and columns are swapped.
+    // That is, the first row of the original matrix becomes the first column of the new matrix,
+    // the second row becomes the second column, and so on.
+
+
+    public int[][] transpose(int[][] matrix) {
+        int[][] transposed = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                transposed[i][j] = matrix[j][i];
+            }
+        }
+        return transposed;
+    }
+}
---
+++ Practice/src/foundation/arrays2d/ArrayTransposeTest.java
@@ -0,0 +1,24 @@
+package foundation.arrays2d;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayTransposeTest {
+
+    @Test
+    void transpose() {
+        ArrayTranspose trans = new ArrayTranspose();
+        int[][] matrix = new int[][] {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        assertArrayEquals(new int[][] {
+                {1,4,7},
+                {2,5,8},
+                {3,6,9}
+        }, trans.transpose(matrix));
+    }
+}
